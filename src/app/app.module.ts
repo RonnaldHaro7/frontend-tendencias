5da90233e3427e21c5d011f5ed7a86ff7188a486
RonnaldHaro7 <rsa.haro@yavirac.edu.ec>
2022-11-26 20:04:31 -0500

    fix bugs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,29 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { NopagesfoundComponent } from './nopagesfound/nopagesfound.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { FooterComponent } from './shared/footer/footer.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { PagesRoutingModule } from './pages/pages-routing.module';
+import { SharedModule } from './shared/shared.module';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent,
-    NopagesfoundComponent,
-    LoginComponent,
-    RegisterComponent,
-    HeaderComponent,
-    FooterComponent,
-    SidebarComponent,
-    BreadcrumbsComponent
+  ],
+  exports: [
+    PagesRoutingModule,
+    SharedModule,
+    AuthModule
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PagesRoutingModule,
+    SharedModule,
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { DashboardComponent } from './dashboard/dashboard.component';
+//import { NopagesfoundComponent } from '../nopagesfound/nopagesfound.component';
 
 @NgModule({
   declarations: [
-    
+    DashboardComponent,
+    // NopagesfoundComponent,
+  ],
+  exports: [
+    DashboardComponent,
+    //NopagesfoundComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    DashboardComponent,
   ]
 })
 export class PagesRoutingModule { }
